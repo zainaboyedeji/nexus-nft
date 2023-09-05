12bdee0104bfc41a68c57f6ad7dbad5cd471fb2f
zainaboyedeji <zainaboyedeji95@gmail.com>
2023-09-05 21:05:21 +0100

    stling of component

--- src/app/components/topListArtist/index.js
+++ src/app/components/topListArtist/index.js
@@ -3,90 +3,101 @@ import alien from "../../../../public/topListArtistImages/alien.png";
 import alienSmall from "../../../../public/topListArtistImages/alien-small.png";
 import pink from "../../../../public/topListArtistImages/pink.png";
 import funds from "../../../../public/topListArtistImages/funds.png";
+import medal from "../../../../public/topListArtistImages/medal.png";
 import ethereum from "../../../../public/ethereum.png";
 
 const TopListArtist = () => {
-    return (
-        <div>
-            <h3>Top List Artist</h3>
-
-            <div className="flex">
-                <div>
-                    <Image src={alien} alt="Alien" />
-                    <div className="flex">
-                        <div>
-                            <Image src={alienSmall} alt="Alien" />
-                            <div>
-                                <h3> Artgangstyle</h3>
-                                <p>Floor Price: 8.6ETH</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <div>
-                                <Image src={alienSmall} alt="Alien" />
-                                <h3>8934.56 ETH</h3>
-                            </div>
-                            <div>
-                                <Image src={alienSmall} alt="Alien" />
-                                <p>+126.23%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div className="flex">
-                        <Image src={pink} alt="Pink" />
-
-                        <div>
-                            <h6>Dareangel</h6>
-                            <div className="flex">
-                                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
-                            </div>
-                            <div className="flex">
-                                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" /> <h6>5.6 ETH</h6>
-                            </div>
-                        </div>
-
-                        <h1>7934.56 ETH</h1>
-                    </div>
-
-                    <div className="flex">
-                        <Image src={pink} alt="Pink" />
-
-                        <div>
-                            <h6>Dareangel</h6>
-                            <div className="flex">
-                                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
-                            </div>
-                            <div className="flex">
-                                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" /> <h6>5.6 ETH</h6>
-                            </div>
-                        </div>
-
-                        <h1>7934.56 ETH</h1>
-                    </div>
-
-                    <div className="flex">
-                        <Image src={pink} alt="Pink" />
-
-                        <div>
-                            <h6>Dareangel</h6>
-                            <div className="flex">
-                                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
-                            </div>
-                            <div className="flex">
-                                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" /> <h6>5.6 ETH</h6>
-                            </div>
-                        </div>
-
-                        <h1>7934.56 ETH</h1>
-                    </div>
-                </div>
+  return (
+    <div className="mt-36">
+      <div>
+        <h3 className="text-4xl mb-10 inline">Top List Artist</h3>
+        <Image src={medal} alt="Medal" className="inline" />
+      </div>
+      <div className="flex justify-between">
+        <div className="bg-gray-100 rounded p-6">
+          <div>
+            <Image src={alien} alt="Alien" className="rounded" />
+          </div>
+          <div className="flex mt-10">
+            <div className="flex justify-between">
+              <div className="">
+                <Image src={alienSmall} alt="Alien-Small" />
+              </div>
+              <div>
+                <h3 className="text-2xl"> Artgangstyle</h3>
+                <p>Floor Price: 8.6ETH</p>
+              </div>
             </div>
+
+            <div>
+              <div>
+                <Image src={ethereum} alt="Ethereum" />
+
+                <h3>8934.56 ETH</h3>
+              </div>
+              <div>
+                <Image src={funds} alt="Funds" />
+                <p>+126.23%</p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div>
+          <div className="flex">
+            <Image src={pink} alt="Pink" />
+
+            <div>
+              <h6>Dareangel</h6>
+              <div className="flex">
+                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
+              </div>
+              <div className="flex">
+                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" />{" "}
+                <h6>5.6 ETH</h6>
+              </div>
+            </div>
+
+            <h1>7934.56 ETH</h1>
+          </div>
+
+          <div className="flex">
+            <Image src={pink} alt="Pink" />
+
+            <div>
+              <h6>Dareangel</h6>
+              <div className="flex">
+                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
+              </div>
+              <div className="flex">
+                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" />{" "}
+                <h6>5.6 ETH</h6>
+              </div>
+            </div>
+
+            <h1>7934.56 ETH</h1>
+          </div>
+
+          <div className="flex">
+            <Image src={pink} alt="Pink" />
+
+            <div>
+              <h6>Dareangel</h6>
+              <div className="flex">
+                <Image src={funds} alt="Funds" /> <span>+126.23%</span>
+              </div>
+              <div className="flex">
+                <h6>Floor Price:</h6> <Image src={ethereum} alt="Alien" />{" "}
+                <h6>5.6 ETH</h6>
+              </div>
+            </div>
+
+            <h1>7934.56 ETH</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopListArtist;
--- src/app/components/trendingNFT/trendingNFT.js
+++ src/app/components/trendingNFT/trendingNFT.js
@@ -4,96 +4,111 @@ import creambox from "../../../../public/creambox.png";
 import ellipsis from "../../../../public/ellipsis.png";
 import brain from "../../../../public/brain.png";
 import box from "../../../../public/box.png";
+import boxtwo from "../../../../public/box2.png";
+import ethereum from "../../../../public/ethereum.png";
+import sculpture from "../../../../public/sculpture.png";
 import "./trendingNFT.css";
 
 const TrendingNFT = () => {
-    return (
-        <div className="mt-36">
-            <h3 className="font-bold text-3xl">Top 3 Trending NFTS</h3>
-            <div className="flex justify-between mt-10">
-                <div className="trendingDiv">
-                    <Image src={blackearth} className="rounded" alt="BlackEarth" />
-                    <h3 className="mt-6 mb-5 font-bold text-xl">Black Earth Grid</h3>
+  return (
+    <div className="mt-36">
+      <h3 className="font-bold text-3xl">Top 3 Trending NFTS</h3>
+      <div className="flex justify-between mt-10">
+        <div className="trendingDiv">
+          <Image src={blackearth} className="rounded" alt="BlackEarth" />
+          <h3 className="mt-6 mb-5 font-bold text-2xl">Black Earth Grid</h3>
 
-                    <div className="flex justify-between">
-                        <div className="flex">
-                            <Image src={creambox} className="rounded-full" alt="CreamBox" /> <span className="mt-3 ml-2">Javier</span>
-                        </div>
-                        <Image src={ellipsis} className="w-5 h-5" alt="Ellipsis" />
-                    </div>
-
-                    <div className="flex mt-5 justify-between">
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                    </div>
-
-                    <div className="mt-5 flex justify-center">
-                        <button className="trendingButton rounded-full">Place a Bid</button>
-                    </div>
-                </div>
-
-                <div className="trendingDiv">
-                    <Image src={brain} alt="Brain" />
-                    <h3 className="mt-5 mb-5 font-bold">Black Earth Grid</h3>
-
-                    <div className="flex justify-between">
-                        <div className="flex">
-                            <Image src={creambox} className="rounded-full" alt="CreamBox" /> <span>Javier</span>
-                        </div>
-                        <Image src={ellipsis} className="w-5 h-5" alt="Ellipsis" />
-                    </div>
-
-                    <div className="flex mt-5 justify-between">
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                    </div>
-
-                    <div className="mt-5 flex justify-center">
-                        <button className="trendingButton rounded-full">Place a Bid</button>
-                    </div>
-                </div>
-
-                <div className="trendingDiv">
-                    <Image src={box} alt="Box" />
-                    <h3 className="mt-5 mb-5 font-bold">Black Earth Grid</h3>
-
-                    <div className="flex justify-between">
-                        <div className="flex">
-                            <Image src={creambox} className="rounded-full" alt="CreamBox" /> <span>Javier</span>
-                        </div>
-                        <Image src={ellipsis} className="w-5 h-5" alt="Ellipsis" />
-                    </div>
-
-                    <div className="flex mt-5 justify-between">
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                        <div>
-                            <p>End in</p>
-                            <h2>1h 50min</h2>
-                        </div>
-                    </div>
-
-                    <div className="mt-5 flex justify-center">
-                        <button className="trendingButton rounded-full">Place a Bid</button>
-                    </div>
-                </div>
+          <div className="flex justify-between">
+            <div className="flex">
+              <Image src={creambox} className="rounded-full" alt="CreamBox" />{" "}
+              <span className="mt-3 ml-2 text-sm">Javier</span>
             </div>
+            <div className="mt-5">
+              <Image src={ellipsis} className="" alt="Ellipsis" />
+            </div>
+          </div>
+
+          <div className="flex mt-6 justify-between">
+            <div>
+              <p className="text-xs">End in</p>
+              <h2 className="text-lg">1h 50min</h2>
+            </div>
+            <div>
+              <p className="text-xs">Top Bid</p>
+              <h2 className="text-lg">
+                <Image src={ethereum} className="" alt="Ethereum" />
+                8.32 ETH
+              </h2>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="trendingDiv">
+          <Image src={brain} alt="Brain" className="rounded" />
+          <h3 className="mt-6 mb-5 font-bold text-2xl">Glowing Brain</h3>
+
+          <div className="flex justify-between">
+            <div className="flex">
+              <Image src={sculpture} className="rounded-full" alt="Sculpture" />{" "}
+              <span className="mt-3 ml-2 text-sm">Maabits</span>
+            </div>
+            <div className="mt-5">
+              <Image src={ellipsis} className="" alt="Ellipsis" />
+            </div>
+          </div>
+
+          <div className="flex mt-6 justify-between">
+            <div>
+              <p className="text-xs">End in</p>
+              <h2 className="text-lg">2h 10min</h2>
+            </div>
+            <div>
+              <p className="text-xs">Top Bid</p>
+              <h2 className="text-lg">
+                <Image src={ethereum} className="" alt="Ethereum" />
+                3.32 ETH
+              </h2>
+            </div>
+          </div>
+
+          <div className="mt-5 flex justify-center">
+            <button className="trendingButton rounded-full w-full">
+              Place a Bid
+            </button>
+          </div>
+        </div>
+
+        <div className="trendingDiv">
+          <Image src={box} className="rounded" alt="Box" />
+          <h3 className="mt-6 mb-5 font-bold text-2xl">Iron Magic Cube</h3>
+
+          <div className="flex justify-between">
+            <div className="flex">
+              <Image src={boxtwo} className="rounded-full" alt="Box-Two" />{" "}
+              <span className="mt-3 ml-2 text-sm">Kondrashova</span>
+            </div>
+            <div className="mt-5">
+              <Image src={ellipsis} className="" alt="Ellipsis" />
+            </div>
+          </div>
+
+          <div className="flex mt-6 justify-between">
+            <div>
+              <p className="text-xs">End in</p>
+              <h2 className="text-lg">3h 30min</h2>
+            </div>
+            <div>
+              <p className="text-xs">Top Bid</p>
+              <h2 className="text-lg">
+                <Image src={ethereum} className="" alt="Ethereum" />
+                4.67 ETH
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TrendingNFT;
